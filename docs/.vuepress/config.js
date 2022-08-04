d336224136ffa7733561bff2c3f22d42902b0b86
XiaofengZeng <798065964@qq.com>
2022-08-04 15:42:13 +0800

    feat(doc): 搭建测试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  title: "kite-notes",
+  description: "some notes about development.",
+  base: "/notes/",
+  // 放在html的head标签中
+  head: [
+    // icon
+    // ["link", { rel: "icon", href: "/notes.jpeg" }],
+    // SEO
+    [
+      "meta",
+      {
+        name: "keywords",
+        content: "笔记",
+      },
+    ],
+  ],
+  themeConfig: {
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    repo: "XiaofengZeng/kite-notes",
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: "Github",
+
+    // 以下为可选的编辑链接选项
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: "XiaofengZeng/kite-notes",
+    docsDir: "docs",
+    docsBranch: "main",
+    // 默认是 false, 设置为 true 来启用
+    editLinks: false,
+    // 默认为 "Edit this page"
+    editLinkText: "帮助我们改善此页面！",
+    // displayAllHeaders: true,
+
+    sidebar: [
+      {
+        title: "环境安装部署",
+        collapsable: false,
+        children: [
+          ["env_deploy/dev_front_pnpm", "[开发-前端]pnpm"],
+          ["env_deploy/soft_gis_geoscene", "[软件-GIS]GeoScene"],
+        ],
+      },
+    ],
+  },
+};
