872c7c6a18c4b6bd6a5914ccf011a56bbafb6ae6
XiaofengZeng <798065964@qq.com>
2022-08-08 20:14:59 +0800

    doc(os): linux-常用指令

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,7 +19,7 @@ module.exports = {
   ],
   themeConfig: {
     // logo：图片，路径根目录为public
-    logo: "icon/nest.png",
+    logo: "/icon/nest.png",
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     repo: "XiaofengZeng/kite-notes",
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
